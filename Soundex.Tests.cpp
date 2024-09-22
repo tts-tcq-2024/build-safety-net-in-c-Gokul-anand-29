005cc8ca257e665602c4d38334a03a6052b25cf0
Gokul-anand-29 <gokulanand200@gmail.com>
2024-09-22 16:36:08 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -9,20 +9,20 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
   ASSERT_STREQ(soundex,"A200");
  
   generateSoundex("gokul", soundex);
-  ASSERT_STREQ(soundex,"G020");
+  ASSERT_STREQ(soundex,"G240");
 
   generateSoundex("good", soundex);
-  ASSERT_STREQ(soundex,"G003"); 
+  ASSERT_STREQ(soundex,"G300"); 
 
   generateSoundex("hwyaeiou", soundex);
   ASSERT_STREQ(soundex,"H000");
 
   generateSoundex("qwertyuiop", soundex);
-  ASSERT_STREQ(soundex,"Q006");
+  ASSERT_STREQ(soundex,"Q630");
 
   generateSoundex("abcde", soundex);
   ASSERT_STREQ(soundex,"A123");
  
   generateSoundex("dessert", soundex);
-  ASSERT_STREQ(soundex,"D020");
+  ASSERT_STREQ(soundex,"D263");
 }
