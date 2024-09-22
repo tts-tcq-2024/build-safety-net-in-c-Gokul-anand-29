f5738f122a1b14c0adee00c9bcdd71d59ca0ee65
Gokul-anand-29 <gokulanand200@gmail.com>
2024-09-22 16:42:19 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -25,4 +25,11 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  
   generateSoundex("dessert", soundex);
   ASSERT_STREQ(soundex,"D263");
+
+ TEST(SoudexTestsuite, ReturnsZerosForDigitOnlyInput) {
+  char soundex[5];
+  generateSoundex("63619156", soundex);
+  EXPECT_STREQ(soundex,"6000");
+  
+}
 }
