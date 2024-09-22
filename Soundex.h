1255a17e89626442d11abd6fde71b461ffc502cb
Gokul-anand-29 <gokulanand200@gmail.com>
2024-09-22 16:33:46 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,54 +1,91 @@
+#ifndef SOUNDEX_H
+#define SOUNDEX_H
+
 #include <ctype.h>
-#include<string.h>
- 
-typedef struct {
-    char sound;
-    char value;
-} SoundexMapping;
- 
-static const SoundexMapping soundexMappings[] = {
-    {'B', '1'}, {'F', '1'}, {'P', '1'}, {'V', '1'},
-    {'C', '2'}, {'G', '2'}, {'J', '2'}, {'K', '2'}, {'Q', '2'},
-    {'S', '2'}, {'X', '2'}, {'Z', '2'},
-    {'D', '3'}, {'T', '3'},
-    {'L', '4'},
-    {'M', '5'}, {'N', '5'},
-    {'R', '6'}
+#include <string.h>
+
+/* Lookup table for Soundex codes for 'A' to 'Z' */
+char soundexMap[26] = {
+    '0', '1', '2', '3', '0', '1', '2', '0', '0', '2', // A,B,C,D,E,F,G,H,I,J
+    '2', '4', '5', '5', '0', '1', '2', '6', '2', '3', // K,L,M,N,O,P,Q,R,S,T
+    '0', '1', '0', '2', '0', '2'                      // U,V,W,X,Y,Z
 };
- 
-#define MAPPING_COUNT (sizeof(soundexMappings) / sizeof(SoundexMapping))
- 
+
+int isUpperCaseLetter(char c) {
+    return c >= 'A' && c <= 'Z';
+}
+
+char toUpperCase(char c) {
+    return toupper(c);
+}
+
 char getSoundexCode(char c) {
-    c = toupper(c);
- 
-    for (int i = 0; i < MAPPING_COUNT; i++) {
-        if (soundexMappings[i].sound == c) {
-            return soundexMappings[i].value;
-        }
+    c = toUpperCase(c);
+    if (isUpperCaseLetter(c)) {
+        return soundexMap[c - 'A'];  /* It Return's mapped code from lookup table */
     }
- 
-        return '0'; 
+    return '0'; /* For non-alphabet characters returns Zero */
 }
- 
-char getValidCode(char currentCode, char lastCode) {
-    if (currentCode != '0' && currentCode != lastCode) {
-        return currentCode;
+
+
+int isSuoundxCode(char code, char previousCode) {
+    return code != '0' && code != previousCode;
+}
+
+/* If valid, this helper function to add a character to the Soundex code */
+void addToSoundex(char *soundex, char code, int *sIndex) {
+    if (isSuoundxCode(code, soundex[*sIndex - 1])) {
+        soundex[(*sIndex)++] = code;
     }
-    return '0';
 }
- 
+
+void initializeSoundex(const char *name, char *soundex, int *sIndex) {
+    soundex[0] = toUpperCase(name[0]);  
+    *sIndex = 1;  /* Initialize the index to the second character */
+}
+
+void padSoundex(char *soundex, int *sIndex) {
+    while (*sIndex < 4) {
+        soundex[(*sIndex)++] = '0';
+    }
+    soundex[4] = '\0'; /* If null-terminate the Soundex code */ 
+}
+
+/* Supporting function to check if Soundex code length is sufficient */
+int isSoundexComplete(int sIndex) {
+    return sIndex >= 4;
+}
+
+int isIndexWithinBounds(int i, int len) {
+    return i < len;
+}
+
+int canContinueProcessing(const char *name, int i, int len, int *sIndex) {
+    return isIndexWithinBounds(i, len) && !isSoundexComplete(*sIndex);
+}
+
+/* Function to process characters and generate Soundex code */
+void processSoundex(const char *name, char *soundex, int *sIndex, int len) {
+    int i = 1;  /* Start from the second character of the input */
+
+    while (canContinueProcessing(name, i, len, sIndex)) {
+        char code = getSoundexCode(name[i]);
+        addToSoundex(soundex, code, sIndex);  
+        i++; 
+    }
+}
+
+
+/* Function to generate the Soundex code */
 void generateSoundex(const char *name, char *soundex) {
-    soundex[0] = toupper(name[0]);
-    soundex[1] = soundex[2] = soundex[3] = '0'; 
-    soundex[4] = '\0'; 
-    char lastCode = '0';
-    int soundexIndex = 1;
-    for (int i = 1; name[i] != '\0' ; ++i) {
-       if (soundexIndex < 4) {  
-char currentCode = getSoundexCode(name[i]);
-        char validCode = getValidCode(currentCode, lastCode);
-            soundex[soundexIndex++] = validCode;
-            lastCode = validCode;
-        }
-    }
+    int len = strlen(name);
+    int sIndex = 0;  
+
+    initializeSoundex(name, soundex, &sIndex);  
+
+    processSoundex(name, soundex, &sIndex, len);  /* Process the characters and generate Soundex code */
+
+    padSoundex(soundex, &sIndex);  /* Pad with '0's to make it of length 4 */
 }
+
+#endif // SOUNDEX_H
